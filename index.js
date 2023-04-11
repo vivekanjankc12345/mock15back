fc6ea2baf51945e858ab4d159c5e52f36049b61c
Vivek Anjan <vivekanjankc12345@gmail.com>
2023-04-11 13:31:24 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express =require("express");
 const app=express();
+var cors = require('cors')
+
+ 
+app.use(cors())
 const {connection}=require("./Config/db")
 const {postmodel}=require("./Model/Postmodel")
 app.use(express.json());
